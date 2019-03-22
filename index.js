13997ce33f3e2164caa4436b88a307fb97c2e906
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-03-22 19:05:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-var dwarfs=["Doc","Dopey","Bashful","Grumpy"]
-
-function dwarfRollCall(dwarfs){
-  var newStr="";
-  dwarfs.forEach(function(name,index){
+var dwarves=["Doc","Dopey","Bashful","Grumpy"]
+function dwarfRollCall(dwarves){
+  let newStr="";
+  dwarves.forEach(function(name, index){
     newStr+=(index+1+". "+name+" ");
   });
   return newStr;
 }
 
-var planeteerCalls=["earth","wind","fire","water","heart"]
-
+var planeteerCalls=["earth","wind","fire","water","heart"];
 function summonCaptainPlanet(planeteerCalls){
-	var array = [];
-  for (var i = 0; i < planeteerCalls.length; i++) {
-   array.push(planeteerCalls[i] + "!");
+  var array=[];
+  for(var i=0; i<planeteerCalls.length; i++){
+    array.push(planeteerCalls[i]+"!");
   }
   return array.map(a=>a.toUpperCase());
 }
 
 function longPlaneteerCalls(planeteerCalls){
-  if (planeteerCalls.length>4){
+  if(planeteerCalls.length>4){
     return true;
   }
   return false;
 }
 
-var cheese=["cheddar","gouda","camembert"]
-
+var cheese= ["cheddar", "gouda", "camember"]
 function findTheCheese(food){
   for (var i=0; i<cheese.length; i++){
-    if (food[i] =="cheddar"
-    || food[i] =="gouda"
-    || food[i] =="camember"){
+    if (food[i]=="cheddar"
+    || food[i]=="gouda"
+    || food[i]=="camember"){
       return food[i];
     }
-    }
-    return "no cheese!"
+  }
+    return "no cheese!";
+  
 }
